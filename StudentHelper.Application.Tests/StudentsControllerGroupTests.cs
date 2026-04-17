d3ed8e1dea9b3659c803f7738531b5c0c0e3527e
hohosha.khr@gmail.com <hohosha.khr@gmail.com>
2026-04-17 10:29:49 +0300

    add group logic for admin

--- StudentHelper.Application.Tests/StudentsControllerGroupTests.cs
+++ StudentHelper.Application.Tests/StudentsControllerGroupTests.cs
@@ -1,8 +1,8 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using StudentHelper.Domain.Entities;
 using StudentHelper.Infrastructure.Data;
@@ -10,110 +10,141 @@ using StudentHelper.Web.Controllers;
 using StudentHelper.Web.Models.Students;
 using Xunit;
 
-namespace StudentHelper.Application.Tests
+namespace StudentHelper.Application.Tests;
+
+public class StudentsControllerGroupTests
 {
-    public class StudentsControllerGroupTests
+    [Fact]
+    public async Task Create_Post_WithNewGroup_CreatesGroupAndRedirects()
     {
-        private static Mock<UserManager<User>> CreateUserManagerMock()
+        await using var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        var options = new DbContextOptionsBuilder<StudentHelperDbContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        await using var context = new StudentHelperDbContext(options);
+        await context.Database.EnsureCreatedAsync();
+
+        var userManagerMock = CreateUserManagerMock();
+
+        userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
+
+        userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
+
+        userManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+
+        userManagerMock.Setup(x => x.AddToRoleAsync(It.IsAny<User>(), "User"))
+            .ReturnsAsync(IdentityResult.Success);
+
+        var controller = new StudentsController(
+            userManagerMock.Object,
+            context,
+            NullLogger<StudentsController>.Instance);
+
+        var model = new CreateStudentViewModel
         {
-            var store = new Mock<IUserStore<User>>();
-            var mgr = new Mock<UserManager<User>>(
-                store.Object, null!, null!, null!, null!, null!, null!, null!, null!);
-            return mgr;
-        }
+            UserName = "student1",
+            Email = "student1@test.com",
+            Password = "Password123!",
+            FirstName = "Ім'я",
+            LastName = "Прізвище",
+            GroupName = "GroupX",
+        };
 
-        [Fact]
-        public async Task Create_Post_WithNewGroup_CreatesGroupAndRedirects()
+        var result = await controller.Create(model);
+
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirect.ActionName);
+
+        var created = await context.Groups.FirstOrDefaultAsync(g => g.Name == "GroupX");
+        Assert.NotNull(created);
+    }
+
+    [Fact]
+    public async Task Edit_Post_WithNewGroup_CreatesGroupAndAssignsToUser()
+    {
+        await using var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        var options = new DbContextOptionsBuilder<StudentHelperDbContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        await using var context = new StudentHelperDbContext(options);
+        await context.Database.EnsureCreatedAsync();
+
+        var user = new User
         {
-            var options = new DbContextOptionsBuilder<StudentHelperDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
+            Id = 1,
+            UserName = "student1",
+            Email = "student1@test.com",
+            FirstName = "Ім'я",
+            LastName = "Прізвище",
+        };
 
-            using var context = new StudentHelperDbContext(options);
+        var userManagerMock = CreateUserManagerMock();
 
-            var userManagerMock = CreateUserManagerMock();
+        userManagerMock.Setup(x => x.FindByIdAsync("1"))
+            .ReturnsAsync(user);
 
-            userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-            userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-            userManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Success);
-            userManagerMock.Setup(x => x.AddToRoleAsync(It.IsAny<User>(), "User"))
-                .ReturnsAsync(IdentityResult.Success);
+        userManagerMock.Setup(x => x.IsInRoleAsync(user, "Admin"))
+            .ReturnsAsync(false);
 
-            var controller = new StudentsController(userManagerMock.Object, context, null);
+        userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
 
-            var model = new CreateStudentViewModel
-            {
-                UserName = "student1",
-                Email = "student1@test.com",
-                Password = "Password123!",
-                FirstName = "Ім'я",
-                LastName = "Прізвище",
-                GroupName = "GroupX"
-            };
+        userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
 
-            var result = await controller.Create(model);
+        User? capturedUser = null;
+        userManagerMock.Setup(x => x.UpdateAsync(It.IsAny<User>()))
+            .Callback<User>(u => capturedUser = u)
+            .ReturnsAsync(IdentityResult.Success);
 
-            // verify redirect
-            var redirect = Assert.IsType<Microsoft.AspNetCore.Mvc.RedirectToActionResult>(result);
-            Assert.Equal("Index", redirect.ActionName);
+        var controller = new StudentsController(
+            userManagerMock.Object,
+            context,
+            NullLogger<StudentsController>.Instance);
 
-            // verify group created in db
-            var created = context.Groups.FirstOrDefault(g => g.Name == "GroupX");
-            Assert.NotNull(created);
-        }
-
-        [Fact]
-        public async Task Edit_Post_WithNewGroup_CreatesGroupAndAssignsToUser()
+        var model = new EditStudentViewModel
         {
-            var options = new DbContextOptionsBuilder<StudentHelperDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
+            Id = 1,
+            UserName = "student1",
+            Email = "student1@test.com",
+            FirstName = "Ім'я",
+            LastName = "Прізвище",
+            GroupName = "GroupY",
+        };
 
-            using var context = new StudentHelperDbContext(options);
+        var result = await controller.Edit(model);
 
-            var user = new User { Id = 1, UserName = "student1", Email = "student1@test.com" };
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirect.ActionName);
 
-            var userManagerMock = CreateUserManagerMock();
+        var created = await context.Groups.FirstOrDefaultAsync(g => g.Name == "GroupY");
+        Assert.NotNull(created);
 
-            userManagerMock.Setup(x => x.FindByIdAsync("1"))
-                .ReturnsAsync(user);
+        Assert.NotNull(capturedUser);
+        Assert.Equal(created!.Id, capturedUser!.GroupId);
+    }
 
-            userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-
-            userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-
-            User? capturedUser = null;
-            userManagerMock.Setup(x => x.UpdateAsync(It.IsAny<User>()))
-                .Callback<User>(u => capturedUser = u)
-                .ReturnsAsync(IdentityResult.Success);
-
-            var controller = new StudentsController(userManagerMock.Object, context, null);
-
-            var model = new EditStudentViewModel
-            {
-                Id = 1,
-                UserName = "student1",
-                Email = "student1@test.com",
-                FirstName = "Ім'я",
-                LastName = "Прізвище",
-                GroupName = "GroupY"
-            };
-
-            var result = await controller.Edit(model);
-
-            var redirect = Assert.IsType<Microsoft.AspNetCore.Mvc.RedirectToActionResult>(result);
-            Assert.Equal("Index", redirect.ActionName);
-
-            var created = context.Groups.FirstOrDefault(g => g.Name == "GroupY");
-            Assert.NotNull(created);
-
-            Assert.NotNull(capturedUser);
-            Assert.Equal(created!.Id, capturedUser!.GroupId);
-        }
+    private static Mock<UserManager<User>> CreateUserManagerMock()
+    {
+        var store = new Mock<IUserStore<User>>();
+        return new Mock<UserManager<User>>(
+            store.Object,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!);
     }
 }
--- StudentHelper.Application.Tests/StudentsControllerTests.cs
+++ StudentHelper.Application.Tests/StudentsControllerTests.cs
@@ -1,240 +1,245 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using Moq;
 using StudentHelper.Domain.Entities;
 using StudentHelper.Web.Controllers;
 using StudentHelper.Web.Models.Students;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace StudentHelper.Tests.Controllers
+namespace StudentHelper.Application.Tests;
+
+public class StudentsControllerTests
 {
-    public class StudentsControllerTests
+    [Fact]
+    public async Task Index_ReturnsViewResult_WithOnlyNonAdminUsers()
     {
-        [Fact]
-        public async Task Index_ReturnsViewResult_WithStudentsList()
+        var users = new List<User>
         {
-            // Arrange
-            var users = new List<User>
-            {
-                new User { Id = 1, UserName = "student1", Email = "student1@test.com" },
-                new User { Id = 2, UserName = "admin1", Email = "admin1@test.com" }
-            }.AsQueryable();
+            new User { Id = 1, UserName = "student1", Email = "student1@test.com" },
+            new User { Id = 2, UserName = "admin1", Email = "admin1@test.com" },
+        }.AsQueryable();
 
-            var userManagerMock = GetUserManagerMock(users);
+        var userManagerMock = GetUserManagerMock(users);
 
-            userManagerMock
-                .Setup(x => x.IsInRoleAsync(It.Is<User>(u => u.Id == 1), "User"))
-                .ReturnsAsync(true);
+        userManagerMock
+            .Setup(x => x.IsInRoleAsync(It.Is<User>(u => u.Id == 1), "Admin"))
+            .ReturnsAsync(false);
 
-            userManagerMock
-                .Setup(x => x.IsInRoleAsync(It.Is<User>(u => u.Id == 2), "User"))
-                .ReturnsAsync(false);
+        userManagerMock
+            .Setup(x => x.IsInRoleAsync(It.Is<User>(u => u.Id == 2), "Admin"))
+            .ReturnsAsync(true);
 
-            var controller = new StudentsController(userManagerMock.Object);
+        var controller = new StudentsController(userManagerMock.Object);
 
-            // Act
-            var result = await controller.Index();
+        var result = await controller.Index();
 
-            // Assert
-            var viewResult = Assert.IsType<ViewResult>(result);
-            var model = Assert.IsAssignableFrom<IEnumerable<User>>(viewResult.Model);
-            Assert.Single(model);
-            Assert.Equal("student1", model.First().UserName);
-        }
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<IEnumerable<User>>(viewResult.Model);
 
-        [Fact]
-        public void Create_Get_ReturnsViewResult()
-        {
-            // Arrange
-            var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
-            var controller = new StudentsController(userManagerMock.Object);
-
-            // Act
-            var result = controller.Create();
-
-            // Assert
-            var viewResult = Assert.IsType<ViewResult>(result);
-            Assert.IsType<CreateStudentViewModel>(viewResult.Model);
-        }
-
-        [Fact]
-        public async Task Create_Post_ValidModel_RedirectsToIndex()
-        {
-            // Arrange
-            var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
-
-            userManagerMock
-                .Setup(x => x.FindByNameAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-
-            userManagerMock
-                .Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-
-            userManagerMock
-                .Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Success);
-
-            userManagerMock
-                .Setup(x => x.AddToRoleAsync(It.IsAny<User>(), "User"))
-                .ReturnsAsync(IdentityResult.Success);
-
-            var controller = new StudentsController(userManagerMock.Object);
-
-            var model = new CreateStudentViewModel
-            {
-                UserName = "student1",
-                Email = "student1@test.com",
-                Password = "Password123!"
-            };
-
-            // Act
-            var result = await controller.Create(model);
-
-            // Assert
-            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
-            Assert.Equal("Index", redirectResult.ActionName);
-        }
-
-        [Fact]
-        public async Task Edit_Get_UserNotFound_ReturnsNotFound()
-        {
-            // Arrange
-            var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
-
-            userManagerMock
-                .Setup(x => x.FindByIdAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-
-            var controller = new StudentsController(userManagerMock.Object);
-
-            // Act
-            var result = await controller.Edit(1);
-
-            // Assert
-            Assert.IsType<NotFoundResult>(result);
-        }
-
-        [Fact]
-        public async Task Delete_UserNotFound_ReturnsNotFound()
-        {
-            // Arrange
-            var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
-
-            userManagerMock
-                .Setup(x => x.FindByIdAsync(It.IsAny<string>()))
-                .ReturnsAsync((User?)null);
-
-            var controller = new StudentsController(userManagerMock.Object);
-
-            // Act
-            var result = await controller.Delete(1);
-
-            // Assert
-            Assert.IsType<NotFoundResult>(result);
-        }
-
-        private static Mock<UserManager<User>> GetUserManagerMock(IQueryable<User> users)
-        {
-            var store = new Mock<IUserStore<User>>();
-            var mock = new Mock<UserManager<User>>(
-                store.Object,
-                null!,
-                null!,
-                null!,
-                null!,
-                null!,
-                null!,
-                null!,
-                null!);
-
-            var userStore = new TestAsyncEnumerable<User>(users);
-            mock.Setup(x => x.Users).Returns(userStore);
-
-            return mock;
-        }
+        Assert.Single(model);
+        Assert.Equal("student1", model.First().UserName);
     }
 
-    public class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    [Fact]
+    public void Create_Get_ReturnsViewResult()
     {
-        public TestAsyncEnumerable(IEnumerable<T> enumerable)
-            : base(enumerable)
-        {
-        }
+        var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
+        var controller = new StudentsController(userManagerMock.Object);
 
-        public TestAsyncEnumerable(System.Linq.Expressions.Expression expression)
-            : base(expression)
-        {
-        }
+        var result = controller.Create();
 
-        public IAsyncEnumerator<T> GetAsyncEnumerator(System.Threading.CancellationToken cancellationToken = default)
-        {
-            return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
-        }
-
-        IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+        var viewResult = Assert.IsType<ViewResult>(result);
+        Assert.IsType<CreateStudentViewModel>(viewResult.Model);
     }
 
-    public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    [Fact]
+    public async Task Create_Post_ValidModel_RedirectsToIndex()
     {
-        private readonly IEnumerator<T> _inner;
+        var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
 
-        public TestAsyncEnumerator(IEnumerator<T> inner)
+        userManagerMock
+            .Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
+
+        userManagerMock
+            .Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
+
+        userManagerMock
+            .Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+
+        userManagerMock
+            .Setup(x => x.AddToRoleAsync(It.IsAny<User>(), "User"))
+            .ReturnsAsync(IdentityResult.Success);
+
+        var controller = new StudentsController(userManagerMock.Object);
+
+        var model = new CreateStudentViewModel
         {
-            _inner = inner;
-        }
+            UserName = "student1",
+            Email = "student1@test.com",
+            Password = "Password123!",
+            FirstName = "Ім'я",
+            LastName = "Прізвище",
+        };
 
-        public T Current => _inner.Current;
+        var result = await controller.Create(model);
 
-        public ValueTask DisposeAsync()
-        {
-            _inner.Dispose();
-            return ValueTask.CompletedTask;
-        }
-
-        public ValueTask<bool> MoveNextAsync()
-        {
-            return new ValueTask<bool>(_inner.MoveNext());
-        }
+        var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectResult.ActionName);
     }
 
-    public class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+    [Fact]
+    public async Task Edit_Get_UserNotFound_ReturnsNotFound()
     {
-        private readonly IQueryProvider _inner;
+        var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
 
-        public TestAsyncQueryProvider(IQueryProvider inner)
-        {
-            _inner = inner;
-        }
+        userManagerMock
+            .Setup(x => x.FindByIdAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
 
-        public IQueryable CreateQuery(System.Linq.Expressions.Expression expression)
-        {
-            return new TestAsyncEnumerable<TEntity>(expression);
-        }
+        var controller = new StudentsController(userManagerMock.Object);
 
-        public IQueryable<TElement> CreateQuery<TElement>(System.Linq.Expressions.Expression expression)
-        {
-            return new TestAsyncEnumerable<TElement>(expression);
-        }
+        var result = await controller.Edit(1);
 
-        public object Execute(System.Linq.Expressions.Expression expression)
-        {
-            return _inner.Execute(expression)!;
-        }
+        Assert.IsType<NotFoundResult>(result);
+    }
 
-        public TResult Execute<TResult>(System.Linq.Expressions.Expression expression)
-        {
-            return _inner.Execute<TResult>(expression);
-        }
+    [Fact]
+    public async Task Delete_UserNotFound_ReturnsNotFound()
+    {
+        var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
 
-        public TResult ExecuteAsync<TResult>(System.Linq.Expressions.Expression expression, System.Threading.CancellationToken cancellationToken = default)
-        {
-            return Execute<TResult>(expression);
-        }
+        userManagerMock
+            .Setup(x => x.FindByIdAsync(It.IsAny<string>()))
+            .ReturnsAsync((User?)null);
+
+        var controller = new StudentsController(userManagerMock.Object);
+
+        var result = await controller.Delete(1);
+
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task Delete_AdminUser_ReturnsForbid()
+    {
+        var user = new User { Id = 1, UserName = "admin1", Email = "admin@test.com" };
+        var userManagerMock = GetUserManagerMock(new List<User>().AsQueryable());
+
+        userManagerMock
+            .Setup(x => x.FindByIdAsync("1"))
+            .ReturnsAsync(user);
+
+        userManagerMock
+            .Setup(x => x.IsInRoleAsync(user, "Admin"))
+            .ReturnsAsync(true);
+
+        var controller = new StudentsController(userManagerMock.Object);
+
+        var result = await controller.Delete(1);
+
+        Assert.IsType<ForbidResult>(result);
+    }
+
+    private static Mock<UserManager<User>> GetUserManagerMock(IQueryable<User> users)
+    {
+        var store = new Mock<IUserStore<User>>();
+
+        var mock = new Mock<UserManager<User>>(
+            store.Object,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!,
+            null!);
+
+        var userStore = new TestAsyncEnumerable<User>(users);
+        mock.Setup(x => x.Users).Returns(userStore);
+
+        return mock;
+    }
+}
+
+public class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+{
+    public TestAsyncEnumerable(IEnumerable<T> enumerable)
+        : base(enumerable)
+    {
+    }
+
+    public TestAsyncEnumerable(System.Linq.Expressions.Expression expression)
+        : base(expression)
+    {
+    }
+
+    public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+        return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+    }
+
+    IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+}
+
+public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+{
+    private readonly IEnumerator<T> inner;
+
+    public TestAsyncEnumerator(IEnumerator<T> inner)
+    {
+        this.inner = inner;
+    }
+
+    public T Current => this.inner.Current;
+
+    public ValueTask DisposeAsync()
+    {
+        this.inner.Dispose();
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<bool> MoveNextAsync()
+    {
+        return new ValueTask<bool>(this.inner.MoveNext());
+    }
+}
+
+public class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+{
+    private readonly IQueryProvider inner;
+
+    public TestAsyncQueryProvider(IQueryProvider inner)
+    {
+        this.inner = inner;
+    }
+
+    public IQueryable CreateQuery(System.Linq.Expressions.Expression expression)
+    {
+        return new TestAsyncEnumerable<TEntity>(expression);
+    }
+
+    public IQueryable<TElement> CreateQuery<TElement>(System.Linq.Expressions.Expression expression)
+    {
+        return new TestAsyncEnumerable<TElement>(expression);
+    }
+
+    public object Execute(System.Linq.Expressions.Expression expression)
+    {
+        return this.inner.Execute(expression)!;
+    }
+
+    public TResult Execute<TResult>(System.Linq.Expressions.Expression expression)
+    {
+        return this.inner.Execute<TResult>(expression);
+    }
+
+    public TResult ExecuteAsync<TResult>(System.Linq.Expressions.Expression expression, CancellationToken cancellationToken = default)
+    {
+        return this.Execute<TResult>(expression);
     }
 }
